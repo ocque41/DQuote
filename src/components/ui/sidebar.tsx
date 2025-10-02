fd7527dd9f5feacddad79f6421653e966c5ab185
ocque41 <73175248+ocque41@users.noreply.github.com>
2025-10-02 13:57:06 +0200

    Update sidebar.tsx

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -259,7 +259,7 @@ const Sidebar = React.forwardRef<
         >
           <div
             data-sidebar="sidebar"
-            className="bg-sidebar group-data-[variant=floating]:border-sidebar-border flex h-full w-full flex-col group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:shadow"
+            className="bg-sidebar border-sidebar-border flex h-full w-full flex-col border-r group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:shadow"
           >
             {children}
           </div>
